f688bdee58e9e8e97dd4a6f462bc3512e153556a
Luan <whiteluan12@yahoo.com>
2023-03-24 15:44:58 +0700

    [Init] Initial the project

---
+++ src/test/java/com/example/examinationweb/ExaminationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examinationweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
